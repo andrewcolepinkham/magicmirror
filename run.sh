dfe6e63defb4b6ecf2637bb380cc24ca1a88232f
andrewcolepinkham <57509695+andrewcolepinkham@users.noreply.github.com>
2022-12-20 18:31:00 -0800

    readme

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-wget -q --spider http://google.com
+for i in {1..5}
+do
+    echo "Checking for internet iteration: $i"
+    wget -q --spider http://google.com
+    if [ $? -eq 0 ]; then
+        echo "Online" >> internet.txt
+        break
+    else
+        echo "Offline" >> internet.txt
+    fi
+    sleep 10
+done
 
-if [ $? -eq 0 ]; then
-    echo "Online" >> internet.txt
-else
-    echo "Offline" >> internet.txt
-fi
 sudo git pull -v >& git_log.txt
 npm run start 2>&1| tee npm.txt
 echo server running
