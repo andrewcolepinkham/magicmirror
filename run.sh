b9804130d6711e16f64188b0d4a1087c3a4de229
andrewcolepinkham <57509695+andrewcolepinkham@users.noreply.github.com>
2022-12-20 18:22:52 -0800

    readme

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 wget -q --spider http://google.com
 
 if [ $? -eq 0 ]; then
-    echo "Online" >> git_log.txt
+    echo "Online" >> internet.txt
 else
-    echo "Offline" >> git_log.txt
+    echo "Offline" >> internet.txt
 fi
 sudo git pull -v >& git_log.txt
 npm run start 2>&1| tee npm.txt
