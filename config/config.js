8e27d84f7f831f43713e4c8fc94ee23a2fd73f2e
andrewcolepinkham <57509695+andrewcolepinkham@users.noreply.github.com>
2023-01-24 22:10:40 -0700

    scheduler

--- config/config.js
+++ config/config.js
@@ -79,7 +79,7 @@ let config = {
 			module: 'MMM-ModuleScheduler',
 			config: {
 				// SHOW MODULES WITH THE CLASS 'daytime_scheduler' AT 06:00 AND HIDE AT 22:00 EVERY DAY
-				global_schedule: {from: '0 20 * * *', to: '0 21 * * *', groupClass: 'photo_scheduler'},
+				global_schedule: {from: '0 6 * * *', to: '0 8 * * *', groupClass: 'photo_scheduler'},
 				global_schedule: {from: '0 22 * * *', to: '0 20 * * *', groupClass: 'photo_scheduler_inverse'}
 			}
 		},
